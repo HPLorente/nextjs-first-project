1aa857cbdcf7a7353976ada73c67e631a22a5dbe
HPLorente <hplorente@gmail.com>
2024-06-09 12:23:44 +0800

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main className="text-center pt-32 px-5">
+      <h1 className="text-4xl md:text-5xl font-bold mb-5">
+        Welcome to my blog
+      </h1>
+      <p className="max-w-[750px] mx-auto leading-8">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
+        doloribus nulla, ratione temporibus dolores vel esse, culpa blanditiis,
+        iure beatae molestias sunt illo dolorum eos ipsam quisquam illum modi
+        hic?
+      </p>
+    </main>
+  );
+}
